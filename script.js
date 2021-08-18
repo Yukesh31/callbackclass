b58f8edb58d08f06a5abf79e520cfe3df2b7b6f5
Yukesh31 <vyukeshkumaran@gmail.com>
2021-08-18 09:07:53 +0530

    second

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-
+setTimeout(function () {
+    document.getElementById("demo").innerHTML = "10";
+    setTimeout(function () {
+      document.getElementById("demo").innerHTML = "9";
+      setTimeout(function () {
+        document.getElementById("demo").innerHTML = "8";
+        setTimeout(function () {
+          document.getElementById("demo").innerHTML = "7";
+          setTimeout(function () {
+            document.getElementById("demo").innerHTML = "6";
+            setTimeout(function () {
+              document.getElementById("demo").innerHTML = "5";
+              setTimeout(function () {
+                document.getElementById("demo").innerHTML = "4";
+                setTimeout(function () {
+                  document.getElementById("demo").innerHTML = "3";
+                  setTimeout(function () {
+                    document.getElementById("demo").innerHTML = "2";
+                    setTimeout(function () {
+                      document.getElementById("demo").innerHTML = "1";
+                      setTimeout(function () {
+                        document.getElementById("demo").innerHTML =
+                          "!!!!!!!!!!!!!!Happy Independence Day !!!!!!!!!!!!!!!";
+                        document.getElementById("imgcontainer").style.visibility =
+                          "visible";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
