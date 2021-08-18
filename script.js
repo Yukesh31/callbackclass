efd15b81055612ec2dcff7ecf52d0495f146657f
Yukesh31 <vyukeshkumaran@gmail.com>
2021-08-18 09:23:26 +0530

    four

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 setTimeout(function () {
-    document.getElementById("demo").innerHTML = "10";
+    document.getElementById("demo").innerHTML = "!!!!10!!!!";
     setTimeout(function () {
-      document.getElementById("demo").innerHTML = "9";
+      document.getElementById("demo").innerHTML = "!!!!9!!!!";
       setTimeout(function () {
-        document.getElementById("demo").innerHTML = "8";
+        document.getElementById("demo").innerHTML = "!!!!8!!!!";
         setTimeout(function () {
-          document.getElementById("demo").innerHTML = "7";
+          document.getElementById("demo").innerHTML = "!!!!7!!!!";
           setTimeout(function () {
-            document.getElementById("demo").innerHTML = "6";
+            document.getElementById("demo").innerHTML = "!!!!6!!!!";
             setTimeout(function () {
-              document.getElementById("demo").innerHTML = "5";
+              document.getElementById("demo").innerHTML = "!!!!5!!!!";
               setTimeout(function () {
-                document.getElementById("demo").innerHTML = "4";
+                document.getElementById("demo").innerHTML = "!!!!4!!!!";
                 setTimeout(function () {
-                  document.getElementById("demo").innerHTML = "3";
+                  document.getElementById("demo").innerHTML = "!!!!3!!!!";
                   setTimeout(function () {
-                    document.getElementById("demo").innerHTML = "2";
+                    document.getElementById("demo").innerHTML = "!!!!2!!!!";
                     setTimeout(function () {
-                      document.getElementById("demo").innerHTML = "1";
+                      document.getElementById("demo").innerHTML = "!!!!1!!!!";
                       setTimeout(function () {
                         document.getElementById("demo").innerHTML =
                           "!!!!!!!!!!!!!!Happy Independence Day !!!!!!!!!!!!!!!";
